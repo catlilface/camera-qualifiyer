5448f6026e94b100a2688f9fd653f478a8e80370
catlilface <aleklim98@gmail.com>
2026-03-18 03:33:46 +0300

    queue subscribers init

--- evaluator/main.py
+++ evaluator/main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from qualifiyer!")
+import asyncio
+
+from faststream import FastStream
+from faststream.rabbit import RabbitBroker
+
+from src.env import PENDING_QUEUE, PROCESS_QUEUE, RABBITMQ_PORT
+
+broker = RabbitBroker(f"amqp://guest:guest@rabbitmq:{RABBITMQ_PORT}")
+app = FastStream(broker)
+
+
+@broker.subscriber(PENDING_QUEUE)
+async def pending(): ...
+
+
+@broker.subscriber(PROCESS_QUEUE)
+async def process(): ...
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(app.run())
